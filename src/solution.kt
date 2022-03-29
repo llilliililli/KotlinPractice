24b2f89b667c7ffcc0fad9055f9d5bb651451a7a
nego_home <wnsrn403@naver.com>
2022-03-29 22:53:32 +0900

    Initial commit

---
+++ src/solution.kt
@@ -0,0 +1,11 @@
+fun main(){
+    val solutionClass = solution()
+    val answer = solutionClass.solution("hello solution")
+    println(answer)
+}
+
+class solution {
+    fun solution(string: String): String{
+        return string
+    }
+}
